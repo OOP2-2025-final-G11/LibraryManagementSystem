7e390e977a1a79dcbdd0079cc3492298589aab2e
nrak126 <daichidaichi126@gmail.com>
2025-12-11 16:22:19 +0900

    注文関連のルートとテンプレートを削除し、不要なコードを整理

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,13 +1,10 @@
 from .user import user_bp
 from .books import books_bp
-from .order import order_bp
 from .book_request import book_request_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
   books_bp,
-  product_bp,
-  order_bp,
   book_request_bp,
 ]
